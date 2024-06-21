b53396a5890475a219246246502b7a12dc728761
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-06-20 20:54:22 -0700

    logic for desktop for arrow change

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 const mobileFeatureBtn = document.querySelector("#mobile-features-list>button");
 const mobileCompanyBtn = document.querySelector("#mobile-company-list>button");
+const desktopFeatureBtn = document.getElementById("features-btn");
+const desktopCompanyBtn = document.getElementById("company-btn");
 
 const toggleSubMenu = function (e) {
   const img = e.currentTarget.querySelector("img");
   e.currentTarget.nextElementSibling.classList.toggle("show-submenu");
+  changeArrowImg(img);
+};
+
+const changeArrowImg = function (img) {
   if (img.src.includes("down")) {
     img.src = img.src.replace("down", "up");
   } else {
@@ -13,3 +19,11 @@ const toggleSubMenu = function (e) {
 
 mobileFeatureBtn.addEventListener("click", toggleSubMenu);
 mobileCompanyBtn.addEventListener("click", toggleSubMenu);
+desktopFeatureBtn.addEventListener("click", (e) => {
+  const img = e.currentTarget.querySelector("img");
+  changeArrowImg(img);
+});
+desktopCompanyBtn.addEventListener("click", (e) => {
+  const img = e.currentTarget.querySelector("img");
+  changeArrowImg(img);
+});
