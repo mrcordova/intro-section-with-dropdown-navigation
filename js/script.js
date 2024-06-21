0a7fd7699bcca514ea30fc3ad42ddca06bf8af4d
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-06-21 13:39:35 -0700

    remove css comments, remove observer in js
    add arrow to background of div and toggle img-arrow-up. order of class in css matters

--- js/script.js
+++ js/script.js
@@ -5,20 +5,6 @@ const desktopCompanyBtn = document.getElementById("company-btn");
 const imgArrowSrc = "./images/icon-arrow-down.svg";
 const imgArrowUpSrc = "./images/icon-arrow-up.svg";
 let count = 0;
-observer = new MutationObserver((changes) => {
-  changes.forEach((change) => {
-    if (change.attributeName.includes("alt")) {
-      const img = change.target;
-      if (img.src.includes("down")) {
-        console.log("go up", img.src);
-        img.setAttribute("src", imgArrowUpSrc);
-      } else {
-        console.log("go down", img.src);
-        img.setAttribute("src", imgArrowSrc);
-      }
-    }
-  });
-});
 
 const toggleSubMenu = function (e) {
   const img = e.currentTarget.querySelector(".img-arrow-up");
@@ -27,12 +13,10 @@ const toggleSubMenu = function (e) {
 };
 const desktopArrowChange = function (e) {
   const img = e.currentTarget.querySelector(".img-arrow-up");
-  // console.log(img);
   changeArrowImg(img);
 };
 const changeArrowImg = function (img) {
   img.classList.toggle("img-arrow-down");
-  // console.log(img.classList);
 };
 
 mobileFeatureBtn.addEventListener("click", toggleSubMenu);
